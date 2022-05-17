0d82cf54e58d5334e038275909442cb0935ab66b
NguyenAnh55 <93259247+NguyenAnh55@users.noreply.github.com>
2022-05-17 11:24:56 +0700

    Add files via upload

---
+++ Assignment.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+using std::string;
+
+class Member{
+private:
+
+string id;
+string name;
+string phone;
+string house_address;
+string house_type;
+double credit;
+double occupier_rate;
+string status;
+string requess;
+
+public:
+
+};
+
+class House{
+private:
+
+string id;
+string owner;
+string location;        // Lack description
+string type;
+double rate;
+string status;
+string requested;
+
+public:
+
+};
+
+class Admin {
+private:
+string name;
+string pwd;
+
+public:
+
+};
