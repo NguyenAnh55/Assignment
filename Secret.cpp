818522aca6329f7fce5fcdb3df497b1c750cdfad
Surllian <73808762+Surllian@users.noreply.github.com>
2022-05-22 21:16:18 +0700

    Update Secret.cpp

--- Secret.cpp
+++ Secret.cpp
@@ -1,4 +1,30 @@
-//
-// Created by user on 22/05/2022.
-//
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
 
+#ifndef UNTITLED3_SECRET_H
+#define UNTITLED3_SECRET_H
+
+class Secret{
+protected:
+    string id;
+    string user_name;
+    string pwd;
+public:
+    Secret();
+    Secret(Secret& s);
+    Secret(string id, string user_name, string pwd);
+    ~Secret();
+    string getID();
+    string getUser_name();
+    string getPwd();
+
+    void setID(string id);
+    void setUser_name(string owner);
+    void setPwd(string location);
+
+    void setAll(string id, string user_name, string pwd);
+};
+
+#endif //UNTITLED3_SECRET_H
