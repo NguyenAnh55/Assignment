59a4cbc18ce9b0e1a644eccef4fbcf604b1205b5
Surllian <73808762+Surllian@users.noreply.github.com>
2022-05-22 20:04:20 +0700

    Update Member.h

--- Member.h
+++ Member.h
@@ -10,8 +10,9 @@ using namespace std;
 
 class Member {
 private:
-    string id, name, phone_number, house_location;
+    string id, name, phone_number, address, house_type;
     double credit, occupier_rating;
+    int request_quantity;
     vector<string>requests;
 public:
     //Default constructor
@@ -19,27 +20,31 @@ public:
     //copy constructor
     Member(Member& c);
     //Parameterized constructor
-    Member(string id, string name, string phone_number, string house_location, double credit, double occupier_rating, vector<string> requests);
+    Member(string id, string name, string phone_number, string address, string house_type, double credit, double occupier_rating, int request_quantity,vector<string> requests);
     // Default destructor
     ~Member();
     //Getters
     string getId();
     string getName();
     string getPhone();
-    string getHouse_local();
+    string getAddress();
+    string getHouse_type();
     double getCred();
     double getOcc_rate();
+    int getR_quan();
     vector<string> getRequests();
     //setters
     void setId(string id);
     void setName(string name);
     void setPhone(string phone_number);
-    void setHouse_local(string house_location);
+    void setAddress(string address);
+    void setHouse_type(string house_type);
     void setCred(double credit);
     void setOcc_rate(double occupier_rating);
+    void setR_quan(int request_quantity);
     void setRequest(vector<string> requests);
     //Set all
-    void setInfo(string id, string name, string phone_number, string house_location, double credit, double occupier_rating, vector<string> requests);
+    void setInfo(string id, string name, string phone_number, string address, string house_type, double credit, double occupier_rating, int request_quantity, vector<string> requests);
     void printInfo();
     void printRequests();
 };
