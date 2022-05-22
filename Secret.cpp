c5fb4e59c7992f1f23a4f19751d6a88d040ad2ae
Surllian <73808762+Surllian@users.noreply.github.com>
2022-05-22 21:16:35 +0700

    Update Secret.cpp

--- Secret.cpp
+++ Secret.cpp
@@ -1,30 +1,56 @@
-#pragma once
-#include <string>
-#include <iostream>
+#include "Secret.h"//
+// Created by user on 22/05/2022.
 using namespace std;
+//Default constructor
+Secret::Secret() {
+    id = "ID";
+    user_name = "User";
+    pwd= "Pass";
+}
+//copy constructor
+Secret::Secret(Secret& s) {
+    this->id = s.id;
+    this->user_name = s.user_name;
+    this->pwd = s.pwd;
+}
+//Parameterized constructor
+Secret::Secret(string id, string user_name, string pwd) {
+    this->id = id;
+    this->user_name = user_name;
+    this->pwd = pwd;
+}
+//Default deconstructor
+Secret::~Secret() {}
 
-#ifndef UNTITLED3_SECRET_H
-#define UNTITLED3_SECRET_H
+//Getter
+string Secret::getID() {
+    return this->id;
+}
+string Secret::getUser_name() {
+    return this->user_name;
+}
+string Secret::getPwd() {
+    return this->pwd;
+}
+//Setter
+void Secret::setID(string id) {
+    this->id = id;
+}
+void Secret::setUser_name(string user_name) {
+    this->user_name = user_name;
+}
+void Secret::setPwd(string pwd) {
+    this->pwd = pwd;
+}
 
-class Secret{
-protected:
-    string id;
-    string user_name;
-    string pwd;
-public:
-    Secret();
-    Secret(Secret& s);
-    Secret(string id, string user_name, string pwd);
-    ~Secret();
-    string getID();
-    string getUser_name();
-    string getPwd();
+//Set Item
+void Secret::setAll(string id, string user_name, string pwd) {
+    setID(id);
+    setUser_name(user_name);
+    setPwd(pwd);
+}
+//Print out
+// Created by user on 21/05/2022.
+//
 
-    void setID(string id);
-    void setUser_name(string owner);
-    void setPwd(string location);
 
-    void setAll(string id, string user_name, string pwd);
-};
-
-#endif //UNTITLED3_SECRET_H
